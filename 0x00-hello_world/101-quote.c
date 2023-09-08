06f9d6a35f241a23a1ee78afe26e732186e0bfac
Kamogelo Ashley <kamoashley33@@gmail.com>
2023-09-07 20:45:31 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main  -  a c program that prints a line to to a standard error
+ * Return 1 (Successful)
+ * /
+ int mai(void)
+{  
+         fput("first c program\" - Jozi ,2023\n",stdout);
+	 return (1);
