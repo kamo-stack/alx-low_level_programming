537ceb414f28e23a37751bad536132609183804d
Kamogelo Ashley <kamoashley33@@gmail.com>
2023-09-07 19:31:41 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -progrsmd prints a program using printf function
+ *Return:(0) success
+ */
+nt main(void)
+{
+	print("with proper grammer,but th out comris a piece of art,\n")
+       return (0);
+}
