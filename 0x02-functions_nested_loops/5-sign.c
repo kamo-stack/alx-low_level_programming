a53eede5fc7b35aa31118fcde34ffbcc24e21be0
Kamogelo Ashley <kamoashley33@@gmail.com>
2023-09-13 05:31:13 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - Prints the sign of a number
- * @c: The number to be checked
+ * @n: The number to be checked
  * Return: 1 for positive num, -1 for negative num and zero for anything else
  */
 
