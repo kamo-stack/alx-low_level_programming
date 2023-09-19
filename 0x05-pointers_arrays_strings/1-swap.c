2fb8e3e5bcb34c58d6e5d38bd1c49264f6eb2b47
Kamogelo Ashley <kamoashley33@@gmail.com>
2023-09-19 05:17:08 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,12 +5,12 @@
  * @a: The first integer to be swapped.
  * @b: The second integer to be swapped.
  *
- * Return: nothing 
+ * Return: nothing
  */
 
 void swap_int(int *a, int *b)
 {
 	int temp = *a;
-	*a = *b
+	*a = *b;
 	*b = temp;
 }
