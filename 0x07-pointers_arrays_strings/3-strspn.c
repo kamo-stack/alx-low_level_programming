6387288a9c12496ae6054dea929be5888214ca4a
Kamogelo Ashley <kamoashley33@@gmail.com>
2023-09-23 18:24:38 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -19,14 +19,16 @@ unsigned int _strspn(char *s, char *accept)
 
 		for (n = 0; accept[n] != '\0'; n++)
 		{
-		if (accept[n] == s[i]);
-		{
-			value++;
-			check = 1;
+			if (accept[n] == s[i])
+			{
+				value++;
+				check = 1;
+			}
 		}
-		}
+
 		if (check == 0)
 			return (value);
 	}
+
 	return (value);
 }
