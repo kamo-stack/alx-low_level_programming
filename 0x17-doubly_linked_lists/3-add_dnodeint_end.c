246473d1c0fbb7e2b6a632f100e4f3d0e323fe88
Kamo Ashley <kamoashley33@gmail.com>
2023-12-07 18:01:07 -0800

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -26,13 +26,14 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		while (h->next != NULL)
 			h = h->next;
-
 		h->next = new;
 	}
 	else
 	{
 		*head = new;
 	}
+
 	new->prev = h;
+
 	return (new);
 }
